6c80b8620b2415c3043ae94c1645bd2e492f066a
Tom Cohen <tomcohent@gmail.com>
2015-07-17 23:39:31 +0300

    Done button is DONE

--- public/js/furrycare.js
+++ public/js/furrycare.js
@@ -507,6 +507,9 @@ furrycareApp.controller('animalCtrl', ['$scope','$rootScope','$http','$cookies',
     $("#profileImage").click(function () {
         $("#imgInp").trigger('click');
     });
+    $("#done_new_animal").click(function () {
+        $("#submit_new_animal").trigger('click');
+    });
 
     function readURL(input) {
         if (input.files && input.files[0]) {
